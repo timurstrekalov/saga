92f3f472555ee71296e9ec3a64378f324b62d697
Timur Strekalov <timur.strekalov@gmail.com>
2012-04-11 22:34:30 +0200

    coverage-maven-plugin: wip

---
+++ coverage-maven-plugin/src/main/java/com/github/timurstrekalov/CoverageMojo.java
@@ -0,0 +1,51 @@
+package com.github.timurstrekalov;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @goal coverage
+ * @phase test
+ * @threadSafe
+ */
+public class CoverageMojo extends AbstractMojo {
+    
+    /**
+     * Location of the file.
+     *
+     * @parameter expression="${project.build.directory}"
+     * @required
+     */
+    private File outputDirectory;
+
+    public void execute() throws MojoExecutionException {
+        File f = outputDirectory;
+
+        if (!f.exists()) {
+            f.mkdirs();
+        }
+
+        File touch = new File(f, "touch.txt");
+
+        FileWriter w = null;
+        try {
+            w = new FileWriter(touch);
+
+            w.write("touch.txt");
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error creating file " + touch, e);
+        } finally {
+            if (w != null) {
+                try {
+                    w.close();
+                } catch (IOException e) {
+                    // ignore
+                }
+            }
+        }
+    }
+}
