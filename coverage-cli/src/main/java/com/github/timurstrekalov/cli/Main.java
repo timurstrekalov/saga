c10fd21844b32c063c63941566e6a143260aef8b
Timur Strekalov <timur.strekalov@gmail.com>
2012-04-12 19:15:48 +0200

    using plexus utils

--- coverage-cli/src/main/java/com/github/timurstrekalov/cli/Main.java
+++ coverage-cli/src/main/java/com/github/timurstrekalov/cli/Main.java
@@ -3,29 +3,43 @@ package com.github.timurstrekalov.cli;
 import com.github.timurstrekalov.CoverageGenerator;
 import com.google.common.collect.ImmutableList;
 import org.apache.commons.cli.*;
-import org.codehaus.plexus.util.DirectoryScanner;
+import org.codehaus.plexus.util.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
 
     public static void main(final String[] args) throws IOException, ParseException {
         final Option baseDirOpt = new Option("b", "base-dir", true, "(required) base directory for test search");
-        final Option testPathOpt = new Option("t", "test-path", true, "(required) Ant-style paths to the tests to run");
+        baseDirOpt.setArgs(1);
+
+        final Option includeOpt = new Option("i", "include", true,
+                "(required) Comma-separated list of Ant-style paths to the tests to run");
+        includeOpt.setArgs(1);
+
+        final Option excludeOpt = new Option("e", "exclude", true,
+                "Comma-separated list of Ant-style paths to the tests to exclude from run");
+        excludeOpt.setArgs(1);
+
         final Option outputDirOpt = new Option("o", "output-dir", true, "(required) the output directory for coverage reports");
+        outputDirOpt.setArgs(1);
 
         final Option outputInstrumentedFilesOpt = new Option("f", "output-instrumented-files", true,
                 "Whether to output instrumented files (default is false)");
+        outputInstrumentedFilesOpt.setArgs(0);
 
         final Option noInstrumentPatternOpt = new Option("n", "no-instrument-pattern", true,
                 "Regular expression patterns to match classes to exclude from instrumentation");
+        noInstrumentPatternOpt.setArgs(Option.UNLIMITED_VALUES);
 
         final Option helpOpt = new Option("h", "help", false, "Print this message");
         final Options options = new Options();
 
         options.addOption(baseDirOpt);
-        options.addOption(testPathOpt);
+        options.addOption(includeOpt);
+        options.addOption(excludeOpt);
         options.addOption(outputDirOpt);
         options.addOption(outputInstrumentedFilesOpt);
         options.addOption(noInstrumentPatternOpt);
@@ -35,24 +49,32 @@ public class Main {
             CommandLineParser parser = new GnuParser();
             CommandLine line = parser.parse(options, args, false);
 
-            if (line.hasOption('h')) {
-                printHelpAndExit(options);
-            }
-
             baseDirOpt.setRequired(true);
-            testPathOpt.setRequired(true);
+            includeOpt.setRequired(true);
             outputDirOpt.setRequired(true);
 
             options.addOption(baseDirOpt);
-            options.addOption(testPathOpt);
+            options.addOption(includeOpt);
             options.addOption(outputDirOpt);
+
+            if (line.hasOption('h')) {
+                printHelpAndExit(options);
+            }
 
             parser = new GnuParser();
             line = parser.parse(options, args);
 
             final CoverageGenerator gen = new CoverageGenerator();
+            final File baseDir = new File(line.getOptionValue('b'));
 
-            gen.setTests(findTests(line.getOptionValue('b'), line.getOptionValues('t')));
+            final List files = FileUtils.getFiles(baseDir, line.getOptionValue('i'), line.getOptionValue('e'));
+            final File[] tests = new File[files.size()];
+
+            for (int i = 0; i < files.size(); i++) {
+                tests[i] = (File) files.get(i);
+            }
+
+            gen.setTests(tests);
             gen.setOutputDir(new File(line.getOptionValue('o')));
 
             final String outputInstrumentedFiles = line.getOptionValue('f');
@@ -79,26 +101,8 @@ public class Main {
     }
 
     private static void printHelpAndExit(final Options options) {
-        new HelpFormatter().printHelp("java -jar coverage.jar", options);
+        new HelpFormatter().printHelp("java -jar coverage.jar", options, true);
         System.exit(1);
-    }
-
-    private static File[] findTests(final String baseDir, final String[] tests) {
-        final DirectoryScanner scanner = new DirectoryScanner();
-
-        scanner.setBasedir(baseDir);
-        scanner.setIncludes(tests);
-        scanner.scan();
-
-        final String[] matches = scanner.getIncludedFiles();
-        final File[] result = new File[matches.length];
-        final File parent = new File(baseDir);
-
-        for (int i = 0; i < matches.length; i++) {
-            result[i] = new File(parent, matches[i]).getAbsoluteFile();
-        }
-
-        return result;
     }
 
 }
