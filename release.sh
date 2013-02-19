c8d453a51b594868ba69fdcd535cb562f94a8b16
Timur Strekalov <timur.strekalov@gmail.com>
2013-02-19 21:56:15 +0100

    added the release script

---
+++ release.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+CUR_VERSION=`mvn help:evaluate -Dexpression=project.version | grep -v '\['`
+
+while getopts "d:r:" opt
+do
+    case "$opt" in
+        d) DEV_VERSION=$OPTARG;;
+        r) REL_VERSION=$OPTARG;;
+    esac
+done
+
+if [[ -n "$DEV_VERSION" && -n "$REL_VERSION" ]]
+then
+    echo "Development version: $DEV_VERSION"
+    echo "Release version: $REL_VERSION"
+
+    mvn release:clean &&                     \
+    mvn release:prepare                      \
+        --batch-mode                         \
+        -Dtag=saga-root-$REL_VERSION         \
+        -DreleaseVersion=$REL_VERSION        \
+        -DdevelopmentVersion=$DEV_VERSION && \
+
+    mvn release:perform
+else
+    echo "Usage: ./release.sh -d <development version> -r <release version>"
+    exit 1
+fi
+ 
+sed -Ei -e "s/(def projectVersion = )('$CUR_VERSION')/\1'$REL_VERSION'/" ./gradle-saga-plugin/build.gradle
+
+cd gradle-saga-plugin
+
+git add build.gradle
+git commit -m "releasing Gradle plugin version $REL_VERSION"
+git push
+
+gradle -Prelease uploadArchives
+
+sed -Ei -e "s/(def projectVersion = )('$REL_VERSION')/\1'$DEV_VERSION'/" build.gradle
+rm build.gradle-e
+
+cd -
